835a1ab7220398988366d57af45b00f83a8a3bce
Daniel Varga <daxendor13@gmail.com>
2020-08-01 11:31:24 +0200

    complete HTML import function (error messages to be added later)

--- scripts/import_dynamic_html.js
+++ scripts/import_dynamic_html.js
@@ -1,16 +1,48 @@
 function initializeDynamicHTML() {
-    let scaleSelector = document.getElementById('note-scale-selector');
-    let valueOfSelectedScale = scaleSelector.options[scaleSelector.selectedIndex].value;
-    let htmlToLoad = "html/" + valueOfSelectedScale + ".html";
+    const scaleSelector = document.getElementById('note-scale-selector');
+    const instrumentSelector = document.getElementById('instrument-selector');
     
-    return loadNoteFinderHTML(htmlToLoad);
+    let selectorID = event.target.getAttribute('ID');
+    
+    if (selectorID === 'instrument-selector') {
+        let valueOfSelectedOption = instrumentSelector.options[instrumentSelector.selectedIndex].value;
+        let htmlToLoad = "html/" + valueOfSelectedOption + ".html";
+    
+        return loadNoteFinderHTML(htmlToLoad, selectorID);
+    } else {
+        let valueOfSelectedOption = scaleSelector.options[scaleSelector.selectedIndex].value;
+        let htmlToLoad = "html/" + valueOfSelectedOption + ".html";
+    
+        return loadNoteFinderHTML(htmlToLoad, selectorID);
+    }
+
 }
 
-async function loadNoteFinderHTML(htmlToLoad) {
+async function loadNoteFinderHTML(htmlToLoad, selectorID) {
+    //fetch htmlToLoad
     const contentElement = await fetch(htmlToLoad);
-    const contentWrapper = document.getElementById('notes');
-    
     const contentElementHTML = await contentElement.text();
 
-    contentWrapper.insertAdjacentHTML("beforeend", contentElementHTML);
+    if (selectorID === 'instrument-selector') {
+        let contentWrapper = document.getElementById('fretboard');
+
+        //remove previously imported HTML
+        contentWrapper.innerHTML = '';
+
+        //import new HTML
+        contentWrapper.insertAdjacentHTML("beforeend", contentElementHTML);
+
+        console.log(selectorID);
+    } else {
+        let contentWrapper = document.getElementById('notes');
+        
+        //remove previously imported HTML
+        contentWrapper.innerHTML = '';
+
+        //import new HTML
+        contentWrapper.insertAdjacentHTML("beforeend", contentElementHTML);
+
+        console.log(selectorID);
+    }
+    
 }
